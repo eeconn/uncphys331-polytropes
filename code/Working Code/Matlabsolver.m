0e43b79cb266a9723aebf33edd3ec63c22c52476
eeconn <eeconn@live.unc.edu>
2014-04-10 22:07:32 -0400

    Revert "Attempted to support multiple n vals"
    This reverts commit 047e4d02605d8f479bf3aa93f5a215ce87888d4d.

--- code/Working Code/Matlabsolver.m
+++ code/Working Code/Matlabsolver.m
@@ -1,15 +1,11 @@
-%Matlab's ode solver
+%Matlab's ode sollver
 %-----------------------------------------------------------------
 clc;
 clear;
 close all;
 %-----------------------------------------------------------------
 
-%n = [0 0.5 1 1.5 2 2.5 3]; % Attempting to make plots with multiple n, but
-% this isn't going to work with how ode45 does function handles. -Erin
-
-% I am going to look into Matlab's file I/O & make .csv files to plot in
-% gnuplot.
+n = 3;
 h = 0.001;
 y0 = 1;
 w0 = 0;
@@ -17,24 +13,13 @@ x0 = 0.0001;
 x1 = 15;
 f = @Mfxy1;
 
-%xarr = cell(size(n));
-%yarr = cell(size(n));
-
 %-----------------------------------------------------------------
 
 Xspan = [x0 x1];
 
-%for i=1:size(n,2)
 [xs,ys] = ode45(f,Xspan,[y0 w0]);
 
-%xarr(i) = {xs(:,1)};
-%yarr(i) = {ys(:,1)};
-%end
-
 figure(2)
-hold all
-%for i=1:size(n)
-%plot(xarr{i},yarr{i})
-plot(xs,ys(:,1));
-%end
+plot(xs,ys(:,1))
+
 % Insert root finder
