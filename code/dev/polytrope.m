cc8656e7eb12d0762a5d8cbb0f3cdfe3bd50779b
Erin Conn <eeconn@live.unc.edu>
2014-04-22 00:14:56 -0400

    adding Matt's mass/radius work

--- code/dev/polytrope.m
+++ code/dev/polytrope.m
@@ -29,85 +29,80 @@ csvwrite('rel.csv',[x2s.',y2s(1,:).',y2s(2,:).']);
 % Plot the solutions on the same graph
 figure('Name','Comparison of non-relativistic and relativistic solutions','NumberTitle','off')
 hold all
+title('Solutions to Dimensionless Relativistic and Non-Relativistic Polytropes')
 xlabel('\xi')
 ylabel('\theta(\xi)')
 
 plot(x1s,y1s(1,:))
 plot(x2s,y2s(1,:))
-legend('\theta_{nr}(\xi), n=1.5','\theta_{r}(\xi), n=3')
-fprintf(1,'Enter to continue...\n');
-pause;
-
-% Density profile
-figure('Name','Density profiles for non-relativistic and relativistic polytropes','NumberTitle','off')
-hold all
-xlabel('\xi')
-ylabel('\rho/\rho_c')
-
-plot(x1s,y1s(1,:).^1.5)
-plot(x2s,y2s(1,:).^3)
-legend('n=1.5 (non-relativistic)','n=3 (relativistic)')
+legend('n = 1.5 polytrope: Non-Relativistic','n = 3 polytrope: Relativistic')
 fprintf(1,'Enter to continue...\n');
 pause;
 
 % Insert Mass-Radius Relationship and Plot
-m1 = zeros(1,size(x1s,2));
-m2 = zeros(1,size(x2s,2));
-% Central density in solar mass/solar radius
-centdens1 = -1/(4*pi)/(1/x1s(end)*y1s(2,end));
-centdens2 = -1/(4*pi)/(1/x2s(end)*y2s(2,end));
-% Ratio of central density to average density (are we using this?)
-rhorat1 = 1/3*x1s(end)/-y1s(2,end);
-rhorat2 = 1/3*x2s(end)/-y2s(2,end);
 
-% still need to find alpha to get mass as a function of R.
-% Using pressure relation to find K
-% SI units:
-h = 6.6261*10^(-34);    %kg m^2/s
-hc = 1.9864*10^(-25);   %kg m^3/s^2
-Msun = 1.98855*10^(30); %kg
-Rsun = 6.96340*10^8;    %m
-Me = 9.1094*10^(-31);   %kg
-Mh = 1.6726*10^(-27);   %kg
-G = 6.67384*10^(-11);   %m^3/kgs^2
+h  = (6.626*10^-34)/(((6.955*10^8)^2)*(1.9891*10^30));
+n2 = 3;
+n1 = 1.5;
+mH = (1.672*10^-27)/(1.9891*10^30);
+me = (9.109*10^-31)/(1.9891*10^30);
+c  = (2.998*10^8)/(6.955*10^8);
+G  = (6.67*10^-11)*((1.9891*10^30)/((6.955*10^8)^3));
+x1e = x1s(end);
+x2e = x2s(end);
+y1e = y1s(2,end);
+y2e = y2s(2,end);
 
-K1 = h^2/(20*Me)*(3/pi)^(2/3)*(1/(2*Mh))^(5/3);
-K2 = hc/8*(3/pi)^(1/3)*(1/(2*Mh))^(4/3);
+Knon = ((h^2)/(20*me))*((3/pi)^(2/3))*((1/(mH*2))^(5/3));
+Krel = ((h*c)/8)*((3/pi)^(1/3))*((1/(mH*2))^(4/3));
 
-% Convert to solar units*
-K1 = K1*Msun^(2/3)/Rsun^4;
-K2 = K2*Msun^(1/3)/Rsun^3;
-G = G*Msun/Rsun^3;
+M1 = (0:0.01:2);
 
-% Central pressure:
-Pc1 = K1*centdens1^(5/3);
-Pc2 = K2*centdens2^(4/3);
+CR1 = (((4*pi)/((x1e^(n1+1))*((-y1e)^(n1-1))))^(1/n1)).*(G/(n1+1)).*(M1.^(1-(1/n1)));
 
-% Calculate alpha:
-alpha1 = sqrt(2.5*Pc1/(4*pi*G*centdens1));
-alpha2 = sqrt(4*Pc2/(4*pi*G*centdens2));
+R1 = (CR1./Knon).^(1/(1-(3/n1)));
 
-% Now we can do the mass/radius relation:
-r1 = alpha1.*x1s;
-r2 = alpha2.*x2s;
-M1=-4*pi*alpha1^3*centdens1.*r1.^2./alpha1^2.*y1s(2,:);
-M2=-4*pi*alpha1^3*centdens2.*r2.^2./alpha2^2.*y2s(2,:);
+figure(2)
+plot(M1,R1);
+title('Radius as a Function of Mass: Non-Relativistic Only');
+xlabel('M/M_{solar}')
+ylabel('R/R_{solar}')
+legend('n = 1.5 polytrope')
+fprintf(1,'Enter to continue...\n');
+pause;
 
-% real data:
-Mreal = [0.46 0.50 0.501 0.53 0.59 0.59 0.604 0.70 0.74 1.000];
-Rreal = [0.13 0.011 0.0136 0.12 0.011 0.015 0.0096 0.0149 0.01245 0.0084];
-Merr  = [0.08 0.05 0.011 0.05 0.06 0.04 0.018 0.12 0.04 0.016];
-Rerr  = [0.002 0.001 0.0002 0.0004 0.001 0.001 0.0004 0.001 0.0004 0.0002];
+R2 = (0:0.001:2); 
 
-figure('Name','Mass/Radius Relation','NumberTitle','off')
-hold all
-xlabel ('M/M_{sun}')
-ylabel ('r/r_{sun}')
+% The term using R2 that 'should' be there is shown, but is not needed
+% because it equals one everywhere for the n = 3 polytrope.
 
-plot(M1,r1)
-plot(M2,r2)
-errorbar(Mreal,Rreal,Rerr,'+')
-% Something's definitely wrong here, our calculated masses aren't even
-% approaching the masses in the provided data table and the radii are much
-% larger. Central density seems to be correct.
-legend('n=1.5 (non-relativistic)','n=3 (relativistic)','Actual data')
+CM2 = (((4*pi)/((x2e^(n2+1))*((-y2e)^(n2-1))))^(1/n2))*(G/(n2+1));%.*(R2.^(-1+(3/n2))) 
+
+% M2 is approximately equal to the Chandrasekhar mass of 1.44 solar masses
+
+M2 = (CM2/Krel)^(1/(-1+(1/n2)));
+
+fprintf('\n')
+fprintf('Chandrasekhar mass M2 = %f\n',M2)
+
+% Combining the pressures. I Just guessed for the constant 
+% (which was 1.38) which we needed to multiply by in order to make the plot
+% go to zero radius at approx. 1.44 solar masses. We could implement a
+% shooting method to find this value, and I am going to try to do that now.
+% The imaginary parts warned of in the command prompt come from the
+% function after it crosses radius = 0 and also at the first value, which
+% is NaN.
+
+M = (0:0.001:2);
+
+R = sqrt(((1.38.*(1./((G^2).*(M.^4))) - (5.4707*10^12)./(M.^(8/3)))./(4.7338*10^16)).*M.^(10/3));
+
+figure(3)
+plot(M,R,M1,R1)
+title('Radius as a Function of Mass');
+xlabel('M/M_{solar}')
+ylabel('R/R_{solar}')
+legend('Combined Polytrope','Non-Relativistic')
+fprintf(1,'Enter to continue...\n');
+pause;
+
